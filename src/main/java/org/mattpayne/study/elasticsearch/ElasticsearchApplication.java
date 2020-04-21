1f0360f825840e9e82512933d8e18514b228a401
Matt Payne <Payne@MattPayne.org>
2020-04-21 13:46:42 -0500

    No warning!  note constructor gets parameter injected is better than autowired.

--- src/main/java/org/mattpayne/study/elasticsearch/ElasticsearchApplication.java
+++ src/main/java/org/mattpayne/study/elasticsearch/ElasticsearchApplication.java
@@ -7,21 +7,18 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
-
-import java.util.List;
 
 @SpringBootApplication
 public class ElasticsearchApplication implements CommandLineRunner {
 
-    @Autowired
-    RestHighLevelClient client;
+    final RestHighLevelClient client;
+
+    public ElasticsearchApplication(RestHighLevelClient client) {
+        this.client = client;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticsearchApplication.class, args);
@@ -30,7 +27,7 @@ public class ElasticsearchApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Running!");
-        SearchQuery searchQuery = new NativeSearchQueryBuilder().build();
+        // https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-search.html
         RequestOptions options=RequestOptions.DEFAULT;
         SearchRequest searchRequest=new SearchRequest();
         SearchSourceBuilder searchSourceBuilder=new SearchSourceBuilder();
