faf92f0f658f6436dda2432c40d0f73ef38cd276
Matt Payne <Payne@MattPayne.org>
2020-04-21 12:38:48 -0500

    Awesome!  This is working! yay!!

--- src/main/java/org/mattpayne/study/elasticsearch/Config.java
+++ src/main/java/org/mattpayne/study/elasticsearch/Config.java
@@ -1,11 +1,8 @@
 package org.mattpayne.study.elasticsearch;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,58 +10,20 @@ import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-import java.net.*;
-
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "org.mattpayne.study.elasticsearch")
 @ComponentScan(basePackages = { "org.mattpayne.study.elasticsearch" })
 public class Config {
 
-    @Value("${elasticsearch.home:/usr/local/Cellar/elasticsearch/5.6.0}")
-    private String elasticsearchHome;
-
-    @Value("es-docker-cluster")
-    private String clusterName;
-
-//    @Bean
-//    public Client client() {
-//        Settings elasticsearchSettings = Settings.builder()
-//                .put("client.transport.sniff", true)
-//                .put("path.home", elasticsearchHome)
-//                .put("cluster.name", clusterName).build();
-//        TransportClient client = new PreBuiltTransportClient(elasticsearchSettings);
-//        try {
-//            client.addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9200));
-//            return client;
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-
+    // Some clues at http://knowledgebasement.com/connect-to-elasticsearch-with-java-and-resthighlevelclient/
     @Bean
-    public Client client() {
-        Settings elasticsearchSettings = Settings.builder()
-                .put("client.transport.sniff", true)
-          //     .put("path.home", elasticsearchHome)
-                .put("cluster.name", clusterName).build();
-        TransportClient client = new PreBuiltTransportClient(elasticsearchSettings);
-        TransportAddress address = null;
-        try {
-            address = new TransportAddress(
-                    InetAddress.getByName("127.0.0.1"),Integer.valueOf(9200));
-            client.addTransportAddress(address);
-            return client;
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public RestHighLevelClient client() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")));
+        return client;
     }
 
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchTemplate(client());
-    }
 }
 
 
--- src/main/java/org/mattpayne/study/elasticsearch/ElasticsearchApplication.java
+++ src/main/java/org/mattpayne/study/elasticsearch/ElasticsearchApplication.java
@@ -1,5 +1,12 @@
 package org.mattpayne.study.elasticsearch;
 
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +21,7 @@ import java.util.List;
 public class ElasticsearchApplication implements CommandLineRunner {
 
     @Autowired
-    ElasticsearchOperations elasticsearchTemplate;
+    RestHighLevelClient client;
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticsearchApplication.class, args);
@@ -24,7 +31,15 @@ public class ElasticsearchApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Running!");
         SearchQuery searchQuery = new NativeSearchQueryBuilder().build();
-        List<String> ids = elasticsearchTemplate.queryForIds(searchQuery);
-        System.out.println(ids);
+        RequestOptions options=RequestOptions.DEFAULT;
+        SearchRequest searchRequest=new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder=new SearchSourceBuilder();
+        BoolQueryBuilder qb = QueryBuilders.boolQuery();
+        qb.must(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.query(qb);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, options);
+        long numHits = searchResponse.getHits().totalHits;
+        System.out.println("numHits="+numHits);
     }
 }
